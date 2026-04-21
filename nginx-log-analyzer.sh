d05965fbe614eb80ab0c800894f9df7a69e5ded7
Subham Kumar <kumarsh2311@gmail.com>
2026-04-21 16:53:38 +0000

    updated nginx-log-analyzer file

--- nginx-log-analyzer.sh
+++ nginx-log-analyzer.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-
+echo "Top 5 IP addresses with the most requests: \n"
 awk '{print $1}' "$1" | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
+
+echo " "
+
+echo "Top 5 most requested paths: \n"
+awk '{print $7}' ../nginx-access.log | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
+
+echo "Top 5 response status codes: \n"
+awk '{print $9}' ../nginx-access.log | sort | uniq -c | sort -rn | head -5 | awk '{print $2 " - " $1 " requests"}'
